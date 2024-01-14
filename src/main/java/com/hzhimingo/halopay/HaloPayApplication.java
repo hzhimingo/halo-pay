682281507e197220bd58dd23c8bdc50b54c2dd81
hzhiming <huangzhimingme@gmail.com>
2024-01-14 20:33:56 +0800

    init project

---
+++ src/main/java/com/hzhimingo/halopay/HaloPayApplication.java
@@ -0,0 +1,13 @@
+package com.hzhimingo.halopay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaloPayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaloPayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hzhimingo/halopay/HaloPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzhimingo.halopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaloPayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
